04bc1fec1cbac0cc5076f385db90b10cc7ccced1
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-11-18 15:29:14 -0300

    test python script created

---
+++ code/test.py
@@ -0,0 +1,31 @@
+from code.database.connection import get_conn
+from code.modules.embeddings import get_all_embeddings_from_a_document
+
+if __name__ == "__main__":
+
+    with get_conn() as conn:
+        with conn.cursor() as cursor:
+
+            sql = """
+                SELECT metadata->>'document_id'
+                FROM embeddings
+                LIMIT 1;
+            """
+
+            cursor.execute(sql)
+            row = cursor.fetchone()
+
+            if not row:
+                raise RuntimeError("Not embedding available.")
+
+            document_id = row[0] if row else None
+
+    embeddings = get_all_embeddings_from_a_document(document_id=document_id)
+
+    print(f'>>> Displaying all available embeddings for document_id="{document_id}"')
+
+    for embedding in embeddings:
+        print(f">>> Embedding with ID [ {embedding.id} ]")
+        print(f"\tContent Length [ {len(embedding.content)} ]")
+        print(">>> Embedding Metadata")
+        print(f"\tChunk Index [ {embedding.metadata.chunk_index} ]")
