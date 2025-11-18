4bcfbc214e2717e20d124b70a2b6e82885e36854
JOÃO PEDRO <pdrojoao.slv@gmail.com>
2025-11-18 10:59:32 -0300

    ingestion script updated

--- code/ingest.py
+++ code/ingest.py
@@ -1,75 +1,12 @@
 import argparse
 import uuid
-from code.config.env_variables import DATABASE_URL, VECTOR_DIMENSION
+from code.config.env_variables import VECTOR_DIMENSION
 from code.models.embedder_model import embedder_model
-from typing import Any, Dict, List
+from code.modules.embeddings import ensure_table, upsert_document
+from code.modules.files import load_file
+from typing import List
 
-import docx
-import pdfplumber
-import psycopg2
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from psycopg2.extras import Json
-
-
-def get_conn():
-    return psycopg2.connect(DATABASE_URL)
-
-
-def ensure_table():
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                f"""
-            CREATE TABLE IF NOT EXISTS embeddings (
-                id          SERIAL PRIMARY KEY,
-                doc_id      TEXT UNIQUE,
-                content     TEXT,
-                metadata    JSONB,
-                embedding   vector({VECTOR_DIMENSION})
-            );
-            """
-            )
-            conn.commit()
-
-
-def upsert_document(
-    doc_id: str, content: str, metadata: Dict[str, Any], embedding: List[float]
-):
-    with get_conn() as conn:
-        with conn.cursor() as cur:
-            cur.execute(
-                """
-                INSERT INTO embeddings (doc_id, content, metadata, embedding)
-                VALUES (%s, %s, %s, %s)
-                ON CONFLICT (doc_id)
-                DO UPDATE SET content = EXCLUDED.content, metadata = EXCLUDED.metadata, embedding = EXCLUDED.embedding;
-                """,
-                (doc_id, content, Json(metadata), embedding),
-            )
-            conn.commit()
-
-
-def read_pdf(path: str) -> str:
-    texts = []
-    with pdfplumber.open(path) as pdf:
-        for p in pdf.pages:
-            texts.append(p.extract_text() or "")
-    return "\n".join(texts)
-
-
-def read_docx(path: str) -> str:
-    doc = docx.Document(path)
-    paragraphs = [p.text for p in doc.paragraphs]
-    return "\n".join(paragraphs)
-
-
-def load_file(path: str) -> str:
-    if path.lower().endswith(".pdf"):
-        return read_pdf(path)
-    elif path.lower().endswith(".docx") or path.lower().endswith(".doc"):
-        return read_docx(path)
-    else:
-        raise RuntimeError("File format not supported.")
 
 
 text_splitter = RecursiveCharacterTextSplitter(
